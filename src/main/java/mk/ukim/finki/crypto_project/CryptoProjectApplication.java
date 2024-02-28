9d3abd6bc842a711633ded2f661d482273de2f2c
MeriDimitrievska <meridimitrievskaaa@gmail.com>
2024-02-28 19:12:27 +0100

    move consumer to another project

--- src/main/java/mk/ukim/finki/crypto_project/CryptoProjectApplication.java
+++ src/main/java/mk/ukim/finki/crypto_project/CryptoProjectApplication.java
@@ -1,9 +1,4 @@
 package mk.ukim.finki.crypto_project;
-
-import mk.ukim.finki.crypto_project.kafka.AvgPriceProducerConfig;
-import mk.ukim.finki.crypto_project.kafka.CryptoCurrencyConsumerConfig;
-import mk.ukim.finki.crypto_project.kafka.MaxPriceProducerConfig;
-import mk.ukim.finki.crypto_project.kafka.MinPriceProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
